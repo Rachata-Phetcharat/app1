91908444706c996ce7c18a4a1f50ecdc241f8aa4
Rachata Phetcharat <mr.bossvpmch59@gmail.com>
2026-01-29 11:05:33 +0700

    context with state

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import React from "react";
 // import "./style.css";
 // import { EventData2 } from "./event-data";
 // import RefsArray from "./refs-array";
+// import Content from "./context-content";
 import { userContext } from "./context";
-import Content from "./context-content";
+import { Header2 } from "./context-header2";
+import Content2 from "./context-content2";
 
 export default function App() {
+  let [user, SetUser] = React.useState("");
   return (
-    <userContext.Provider value={"Tom Jerry"}>
-      <Content />
+    <userContext.Provider value={[user, SetUser]}>
+      <Header2 />
+      <Content2 />
     </userContext.Provider>
   );
 }
