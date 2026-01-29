c177bae388aa58ce30bd1229824284a3d5cd1773
Rachata Phetcharat <mr.bossvpmch59@gmail.com>
2026-01-29 10:06:44 +0700

    Context

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
-import MessageBox from "./state-fumc";
+// import MessageBox from "./state-fumc";
 // import "./style.css";
 // import { EventData2 } from "./event-data";
 // import RefsArray from "./refs-array";
+import { userContext } from "./context";
+import Content from "./context-content";
 
 export default function App() {
-  return <MessageBox />;
+  return (
+    <userContext.Provider value={"Tom Jerry"}>
+      <Content />
+    </userContext.Provider>
+  );
 }
