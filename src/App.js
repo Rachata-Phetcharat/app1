cb8cd1c16bd9d1a37e30a1a4a55a20da3eb90aaa
Rachata Phetcharat <mr.bossvpmch59@gmail.com>
2026-02-19 11:46:38 +0700

    input text:

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ import React from "react";
 // import { Header2 } from "./context-header2";
 // import Content2 from "./context-content2";
 // import Router2 from "./router2";
-import Router3 from "./router3";
+// import Router3 from "./router3";
+import FormText from "./form-text";
 
 export default function App() {
-  return <Router3 />;
+  return <FormText />;
 }
