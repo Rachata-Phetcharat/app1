469e7a9e989e5d03529cb2ef5ff3bd7464e2d4fb
Rachata Phetcharat <mr.bossvpmch59@gmail.com>
2026-02-26 10:30:38 +0700

    form-select-file

--- src/App.js
+++ src/App.js
@@ -9,8 +9,9 @@ import React from "react";
 // import Content2 from "./context-content2";
 // import Router2 from "./router2";
 // import Router3 from "./router3";
-import FormText from "./form-text";
+// import FormText from "./form-text";
+import FormSelectFile from "./form-select-file";
 
 export default function App() {
-  return <FormText />;
+  return <FormSelectFile />;
 }
